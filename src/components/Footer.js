fb78d55ef14da3f957e3bbc1a557d294d42cb12b
Drayfer <devlydexter@gmail.com>
2021-08-21 10:18:10 +0300

    Студенты в футере

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,12 @@ export const Footer = () => {
     const { students, lastWeekDays, nextWeekDays, options, weekTab } = useApp()
 
     let currentLessons = 0;
+    const hideStudents = students.reduce((sum, current) => { 
+        return current.hide ? sum + 1 : sum
+    }, 0);
+    const activeStudents = students.reduce((sum, current) => { 
+        return !current.hide ? sum + 1 : sum
+    }, 0);
 
     switch(weekTab) {
         case 'active': 
@@ -29,8 +35,12 @@ export const Footer = () => {
 
 
 
+
+console.log(hideStudents)
     return (
         <div className={styles.container}>
+        <div style={{marginRight: '1rem'}}>Студенты активные: {activeStudents}</div>
+        <div style={{marginRight: '1rem'}}>В отпуске: {hideStudents}</div>
         <div style={{marginRight: '1rem'}}> {weekTab === 'active' ? 'На этой неделе: ': weekTab === 'past' ? 'На прошлой неделе: ' : 'На следующей неделе: '} {currentLessons}</div>
         <div>Всего проведено: {options.countLessons}</div>
         </div>
